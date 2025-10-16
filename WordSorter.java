4362bdcaa7e438594f83344d51bd4001892653ab
LeoWang-LW <yuzhewang2017@gmail.com>
2025-10-16 16:48:06 +0000

    Not Finished 10/16/25

--- WordSorter.java
+++ WordSorter.java
@@ -5,6 +5,9 @@ public class WordSorter
 {
 	public static void main(String[] args) 
 	{
+
+		Scanner input = new Scanner(System.in);
+
 		Scanner in = new Scanner(System.in); 
 		try
 		{
@@ -38,16 +41,26 @@ public class WordSorter
 		
 			int index = word.substring(0,1).compareTo("a"); 
 			wordList[index].add(word); 
+			boolean exists = false; 
 
 			for(int i = 0; i < wordList.length; i++){
-				for(String temp: wordList.get(i))
-					if(wordList[i].equals(temp))
-						wordList[i].remove(i);
+				for(int j = 0; j < wordList[i].size(); j++){
+					if(wordList[i].get(j).equals(word) && exists == false)
+						exists = true;
+					else if( wordList[i].get(j).equals(word) && exists == true)
+						wordList[i].remove(j); 
+				}
 			}
+		}
+
+		boolean stop = false; 
+
+		while(stop == false){
 			
 		}
+
 		for (int x = 0; x < wordList.length; x++) {
-		System.out.println(wordList[x]);
+		System.out.println(wordList[x]); 
 		}
 
 	}
